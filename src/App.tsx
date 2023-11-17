c2fb3109de5e72e065c1a40f8eceb6c9b8e9894c
Iznobad <brandon.cebi.pro@gmail.com>
2023-11-17 13:44:51 +0100

    add login and register pages, router, and tooltip login

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Menu from "./components/partials/Menu";
 import TopBar from "./components/partials/TopBar";
 import Home from "./screens/Home";
 import Footer from "./components/partials/Footer";
+import { Route, Routes } from "react-router-dom";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -14,7 +17,11 @@ function App() {
     <>
       <TopBar toggleMenu={toggleMenu} />
       <Menu isMenuOpen={isMenuOpen} />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
     </>
   );
--- src/components/partials/TopBar.tsx
+++ src/components/partials/TopBar.tsx
@@ -3,11 +3,19 @@ import { RiArrowDownSLine, RiArrowRightSLine } from "react-icons/ri";
 import { BsCart3, BsPersonFill } from "react-icons/bs";
 import Logo from "../../assets/images/logo.png";
 import SearchBar from "./SearchBar";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 type TopBarProps = {
   toggleMenu: () => void;
 };
 function TopBar({ toggleMenu }: TopBarProps) {
+  const [openLogin, setOpenLogin] = useState(false);
+
+  const toggleLogin = () => {
+    setOpenLogin(!openLogin);
+  };
+
   return (
     <header className="w-full bg-primaryContrast">
       <div className="flex items-center gap-2 lg:container lg:mx-auto lg:h-[80px] lg:gap-6">
@@ -18,21 +26,44 @@ function TopBar({ toggleMenu }: TopBarProps) {
           <PiTextAlignJustify size={40} color="white" />
         </div>
         <div className="px-[5px] py-[1px] border-r border-primary flex-grow lg:grow-0 lg:border-r-0">
-          <img
-            src={Logo}
-            alt="E-commerce website logo"
-            className="max-w-full h-12 mx-auto"
-          />
+          <Link to="/">
+            <img
+              src={Logo}
+              alt="E-commerce website logo"
+              className="max-w-full h-12 mx-auto"
+            />
+          </Link>
         </div>
         <div className="hidden flex-grow lg:block">
           <SearchBar />
         </div>
         <div className="text-white border-primary border-r p-[5px] lg:border-r-0 lg:flex lg:flex-col-reverse lg:items-center">
-          <p className="inline lg:hidden">
+          <Link to="/login" className="inline lg:hidden">
             Se connecter <RiArrowRightSLine className="inline" />
-          </p>
-          <p className="hidden lg:block">
+          </Link>
+          <p
+            className="hidden lg:block relative cursor-pointer"
+            onClick={toggleLogin}
+          >
             Identifiez-vous <RiArrowDownSLine className="inline" />
+            <div
+              className={`${
+                openLogin ? "block" : "hidden"
+              } absolute top-8 w-[300px] left-1/2 transform -translate-x-1/2 bg-[#d9d9d9] text-black text-center p-5 z-20 rounded before:content-[''] before:w-0 before:h-0 before:border-[10px] before:border-l-transparent before:border-r-transparent before:border-b-[10px] before:border-t-transparent before:border-solid before:border-[#d9d9d9] before:absolute before:top-[-20px] before:left-1/2 before:transform before:-translate-x-1/2`}
+            >
+              <Link
+                to="/login"
+                className="bg-secondary w-5/6 block mx-auto rounded py-2 px-4"
+              >
+                Connexion
+              </Link>
+              <div className="mt-4">
+                Nouveau client ?{" "}
+                <Link to="/register" className="underline text-blue-600">
+                  Créer un compte
+                </Link>
+              </div>
+            </div>
           </p>
           <BsPersonFill className="inline lg:hidden" size={40} />
         </div>
